4baa537cb09d3f7c0c168731208b6ced40159de7
roshannoonoottil <roshannoonoottil@gmail.com>
2024-09-20 09:31:39 +0530

    7th commit
     React Controlled Component
    in the previous the data is only hiding and showing now its inside the render function.

--- src/list/List.js
+++ src/list/List.js
@@ -26,44 +26,68 @@ class List extends React.Component {
         super(props);
 
         this.state = {
-            data:arr
+            data:arr,
+            activeState: 'all'
         };
     }
 
     onListChange = (evt)=>{
         console.log(evt.target.value);
         const value = evt.target.value
-        const newList = arr.filter((item)=>{
+        // const newList = arr.filter((item)=>{
 
-            if(value === 'all'){
+        //     if(value === 'all'){
+        //         return true
+        //     }
+        //     if(value === 'active'){
+        //         return item.isActive === true;
+        //     }
+        //     if(value === 'not-active'){
+        //         return item.isActive === false;
+        //     }
+        //     return false
+
+        // });
+
+        // console.log(newList);
+        
+        // this.setState({
+        //     data:newList
+        // })
+        
+        this.setState({
+            activeState:value
+        })
+        
+    }
+
+    // if we are using normal function we need to use bind(this) for arrow function no needed
+    render(){
+
+         const {
+            data,
+            activeState
+         } = this.state;
+         const newList = data.filter((item)=>{
+
+            if(activeState === 'all'){
                 return true
             }
-            if(value === 'active'){
+            if(activeState === 'active'){
                 return item.isActive === true;
             }
-            if(value === 'not-active'){
+            if(activeState === 'not-active'){
                 return item.isActive === false;
             }
             return false
 
         });
 
-        console.log(newList);
-        
-        this.setState({
-            data:newList
-        })
-        
-        
-    }
-
-    // if we are using normal function we need to use bind(this) for arrow function no needed
-    render(){
         return(
             <Tools onAction={this.onListChange}> 
                 <div className='app-list'>
                     {
-                        this.state.data.map((obj) =>{
+                        newList.map((obj) =>{
                         return  <ListItem key={obj.title} onAction={this.myAction} title={obj.title} desc={obj.desc} isActive={obj.isActive}/>
                         })
                     }
